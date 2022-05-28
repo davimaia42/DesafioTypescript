aa3ed94bab48a1c94af524d7034a77e3c70bc9f4
davimaia42 <davi.mendonca.maia@gmail.com>
2022-05-28 18:48:25 -0300

    feat(front-end): create initial structure using Next.js

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+
+/**
+ * Home Page
+ * @return {JSX.Element}: JSX code for the list page
+ */
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Products</title>
+            </Head>
+
+            <main>
+                <h1>Welcome</h1>
+            </main>
+        </div>
+    );
+}
